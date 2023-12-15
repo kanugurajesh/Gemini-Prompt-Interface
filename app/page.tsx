9ab71080ee5b7c65338fd9e228b727fd266b0d5c
kanugurajesh <245122733172@mvsrec.edu.in>
2023-12-15 19:37:53 +0530

    Updating the ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useState } from "react";
-import ReactMarkdown from "react-markdown";
+import { useState, useEffect } from "react";
+import Markdown from 'react-markdown'
 import { Input } from "@/components/ui/input"
 import { Send } from "lucide-react";
 import { Card } from "@/components/ui/card";
@@ -11,8 +11,13 @@ export default function Home() {
   
   const [prompt, setPrompt] = useState("");
   const [response, setResponse] = useState("");
+  const [output, setOutput] = useState("");
 
   const onSubmit = async () => {
+
+    // clear the output
+    setOutput("");
+
     // create a post request to the /api/chat endpoint
     const response = await fetch("api/chat", {
       method: "POST",
@@ -30,13 +35,25 @@ export default function Home() {
     setResponse(data.text);
   };
 
+  useEffect(() => {
+    // update the response character by character in the output
+    if (response.length === 0) return;
+    
+    for (let i = 0; i < response.length; i++) {
+      setTimeout(() => {
+        setOutput((prev) => prev + response[i]);
+      }, i * 5);
+    }
+
+  }, [response]);
+
   return (
     <main className="flex flex-col justify-center items-center h-screen gap-4">
       <div className="relative">
         <Input
           type="text"
           placeholder="prompt"
-          className={cn("w-[380px] h-[45px]")}
+          className={cn("w-[400px] h-[45px]")}
           onChange={(e) => {
             setPrompt(e.target.value);
           }}
@@ -45,8 +62,10 @@ export default function Home() {
           <Send />
         </button>
       </div>
-      <Card className={cn("w-[800px] h-[380px] p-5")}>
-        <ReactMarkdown>{response}</ReactMarkdown>
+      <Card className={cn("w-3/6 p-5 whitespace-normal")}>
+        <div style={{ whiteSpace: "wrap" }}>
+          <Markdown className={cn("w-full h-full")}>{`${output}`}</Markdown>
+        </div>
       </Card>
     </main>
   );
